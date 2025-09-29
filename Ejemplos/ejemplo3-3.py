38e3aa298adb1daea80058783944118b3101d2b8
Isaías Izquierdo Iglesias <iizqigl898@g.educaand.es>
2025-09-29 12:44:49 +0200

    ejemplos

---
+++ Ejemplos/ejemplo3-3.py
@@ -0,0 +1,5 @@
+
+nombre = input("introduzaca su nombre:")
+edad = input("introduzca su edad:")
+
+print(F"Su nombre es {nombre} y su edad es {edad} ") 
---
+++ Ejemplos/ejemplo3-4.py
@@ -0,0 +1,11 @@
+#nombre = input("introduzaca su nombre:")
+#edad = input("introduzca su edad:")
+
+#print(F"Su nombre es {nombre} y su edad es {edad} ") 
+
+#FPRINT
+num1 = 4
+num2 = 2
+nombre = "Juan"
+
+print(f"Hola {nombre}, el resultado de sumar {num1} y {num2} es: {num1 + num2 }")
