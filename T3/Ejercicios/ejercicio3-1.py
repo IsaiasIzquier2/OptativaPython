d01dcfc47713497eb0263b29f989898972c1a8cb
Isaías Izquierdo Iglesias <iizqigl898@g.educaand.es>
2025-09-29 17:40:54 +0200

    T3 ejercicios 1y2

---
+++ T3/Ejercicios/ejercicio3-1.py
@@ -0,0 +1,7 @@
+
+nombre = "Michael Jordan"
+edad = 50
+media_puntos = 28.5
+activo = False                                                                                                                                                                  
+
+print(f"{nombre} tiene {edad} años, tiene una media de {media_puntos} puntos.: {activo}")
---
+++ T3/Ejercicios/ejercicio3-2.py
@@ -0,0 +1,9 @@
+
+nombre = input("Introduzca su nombre:")
+numDNI = input("Introduzca su número de DNI sin letra:")
+letrDNI = str.upper(input("Introduzca su letra de DNI:"))
+
+print(f"""
+      Nombre:{nombre}
+      DNI:{numDNI + letrDNI}
+      """)
