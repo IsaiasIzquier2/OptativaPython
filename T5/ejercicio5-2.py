63d72ee3f3166547301490f7f42eee111b4e0513
Isaías Izquierdo Iglesias <iizqigl898@g.educaand.es>
2025-10-06 11:57:30 +0200

    .

--- T5/ejercicio5-2.py
+++ T5/ejercicio5-2.py
@@ -1,10 +1,12 @@
 num1 = int(input("Introduce un número entero"))
 num2 = int(input("Introduce otro número entero"))
-suma = 0
+repeticiones = num2 - num1 + 1
+suma = num1
 
-for n in range(num1, num2):
+for n in range(repeticiones):
     suma = suma + n
     
-    print(suma)
+    
+print(suma)
 
 
