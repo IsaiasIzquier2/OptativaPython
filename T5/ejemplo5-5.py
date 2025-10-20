90f34361622c50a83390ff8c7abe4de7f715c1cd
Isaías Izquierdo Iglesias <iizqigl898@g.educaand.es>
2025-10-20 13:13:06 +0200

    ejercicio5-6,7

---
+++ T5/ejemplo5-5.py
@@ -0,0 +1,8 @@
+
+contador = 0
+
+while ( ):
+    contador = contador+1
+    print(f"Iteración de número {contador}")
+
+print("fin")
---
+++ T5/ejercicio5-6.py
@@ -0,0 +1,7 @@
+
+iname = input("Usuario:")
+ikey = int(input("Contraseña:"))
+
+name = "Isaías"
+key = 1234
+
